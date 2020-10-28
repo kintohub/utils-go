c44d6b098baa581151a2dca0c310fff9401b9a96
Nadeem Khedr <nadeemkhedr@gmail.com>
2020-10-28 08:34:34 +0200

    ✨ feat(github): add all the logic to the github module and expose public methods only  (#13)
    * update github client to house all the github methods that we use
    
    * refactor github to use base url inside of the package
    
    * accept client id and use it for auth
    
    * refactor use of error messages, update use of temp accept headers for some requests
    
    * remove debugging comment
    
    * rename base urls to have better names
    
    * rename Id to ID to be consistent with the community
    
    * remove generate installation id
    
    * add docs and update formatting
    
    * better error responses and logging

--- github/github.go
+++ github/github.go
@@ -2,55 +2,184 @@ package github
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
+	"net/url"
+	"strings"
+
 	"github.com/kintohub/utils-go/klog"
 	"github.com/valyala/fasthttp"
-	"strings"
-	"time"
 )
 
 type GithubInterface interface {
-	CallGithub(endpoint, verb, params, authToken string) ([]byte, error)
-	GenerateInstallationToken(installationId string) (string, error)
-	GenBearer(jwtToken string) string
+	GetListRepos(page int32, installationId, userAccessToken string) (*GithubRepositories, error)
+	GetUserInformation(userAccessToken string) (*GithubUserInfo, error)
+	CreateGithubAppToken(installationId string) (string, error)
+	GetUserAccessToken(code string) (string, error)
 }
 
 type github struct {
-	baseUrl       string
-	acceptHeader  string
-	appID         string
-	appPrivateKey []byte
+	appClientID     string
+	appClientSecret string
 }
 
-func New(baseUrl, acceptHeader, appID string, appPrivateKey []byte) GithubInterface {
+var (
+	BASE_API_URL             = "https://api.github.com"
+	BASE_URL                 = "https://github.com"
+	TEMP_ACCEPT_HEADER_VALUE = "application/vnd.github.machine-man-preview+json"
+)
+
+func New(appClientID, appClientSecret string) GithubInterface {
 	g := &github{
-		baseUrl:       baseUrl,
-		acceptHeader:  acceptHeader,
-		appID:         appID,
-		appPrivateKey: appPrivateKey,
+		appClientID:     appClientID,
+		appClientSecret: appClientSecret,
 	}
 	return g
 }
 
+// code is a temp string provided by github, we will send it along with the github client id and secret to get a
+// user access token for that user
+func (g *github) GetUserAccessToken(code string) (string, error) {
+	type githubUserAccessRequest struct {
+		ClientID     string `json:"client_id"`
+		ClientSecret string `json:"client_secret"`
+		Code         string `json:"code"`
+	}
+
+	requestBody := githubUserAccessRequest{
+		ClientID:     g.appClientID,
+		ClientSecret: g.appClientSecret,
+		Code:         code,
+	}
+
+	githubUrl := getUrl(BASE_URL, "/login/oauth/access_token", "")
+	body, err := g.callGithub(githubUrl, "POST", "", false, requestBody)
+	bodyStr := string(body)
+
+	if err != nil {
+		klog.ErrorfWithErr(err, "Github error getting access token: %v", bodyStr)
+		return "", errors.New("Error getting access token from github.")
+	}
+	// Response is encoded as x-www-form-urlencoded so we parse it
+	// as a url segment by adding `?` at the beginning
+	parsedUrl, err := url.Parse("?" + bodyStr)
+	parseErr := errors.New("Error parsing github response.")
+	if err != nil {
+		klog.Errorf("Github error parsing get token response: %v", bodyStr)
+		return "", parseErr
+	}
+
+	parsedQuery, err := url.ParseQuery(parsedUrl.RawQuery)
+	if err != nil {
+		klog.Errorf("Github error parsing get token response: %v", bodyStr)
+		return "", parseErr
+	}
+
+	accessToken := parsedQuery["access_token"]
+	if len(accessToken) == 0 {
+		klog.Errorf("Github error parsing response: %v", bodyStr)
+		return "", parseErr
+	}
+	return accessToken[0], nil
+}
+
+type GithubRepository struct {
+	CloneURL      string `json:"clone_url"`
+	Private       bool   `json:"private"`
+	DefaultBranch string `json:"default_branch"`
+}
+
+type GithubRepositories struct {
+	Repositories []GithubRepository `json:"repositories"`
+	TotalCount   int                `json:"total_count"`
+}
+
+func (g *github) GetListRepos(page int32, installationId, userAccessToken string) (*GithubRepositories, error) {
+	query := ""
+	if page != 0 {
+		query = fmt.Sprintf("page=%d", page)
+	}
+	endpoint := fmt.Sprintf("/user/installations/%v/repositories", installationId)
+	url := getUrl(BASE_API_URL, endpoint, query)
+
+	body, err :=
+		g.callGithub(url, "GET", genAuthToken(userAccessToken), true, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting repo list from github. %v", err)
+	}
+
+	githubRepos := GithubRepositories{}
+	err = json.Unmarshal(body, &githubRepos)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing github response. %v", err)
+	}
+	return &githubRepos, nil
+}
+
+type GithubUserInfo struct {
+	Email    string `json:"email"`
+	Username string `json:"login"`
+}
+
+func (g *github) GetUserInformation(userAccessToken string) (*GithubUserInfo, error) {
+	url := getUrl(BASE_API_URL, "/user", "")
+	body, err :=
+		g.callGithub(url, "GET", genAuthToken(userAccessToken), true, nil)
+	bodyStr := string(body)
+	if err != nil {
+		klog.ErrorfWithErr(err, "Github error getting user info: %v", bodyStr)
+		return nil, errors.New("Error getting user info from github.")
+	}
+
+	userInfo := GithubUserInfo{}
+	err = json.Unmarshal(body, &userInfo)
+	if err != nil {
+		klog.ErrorfWithErr(err, "Github parsing github response for getting user info: %v", bodyStr)
+		return nil, errors.New("Error parsing github response for getting user info.")
+	}
+	return &userInfo, nil
+}
+
+func (g *github) CreateGithubAppToken(token string) (string, error) {
+	return fmt.Sprintf("x-access-token:%s", token), nil
+}
+
+func getUrl(baseUrl, endpoint, query string) string {
+	return fmt.Sprintf("%s/%s?%s",
+		baseUrl, strings.TrimPrefix(endpoint, "/"), strings.TrimPrefix(query, "?"))
+}
+
 // A function that calls the github api, uses github base url
-// accepts an endpoint that starts with "/" (ex: /app/installation)
-// accept query that will be set after "?" (ex: page=2&per_page=50)
-// sets the default "Accept" header to the one used by github apps
-// authToken is the full auth token not just the value (ex: authToken="Bearer {token}")
-func (g *github) CallGithub(endpoint, verb, query, authToken string) ([]byte, error) {
-	fullUrl := fmt.Sprintf("%s/%s?%s",
-		g.baseUrl, strings.TrimPrefix(endpoint, "/"), strings.TrimPrefix(query, "?"))
-	klog.Debugf("Full Github URL: %v, Auth Token: %v", fullUrl, authToken)
+// url the github url
+// verb the http verb
+// authHeaderValue the "Authorization" header value, the value has to be formated with "Bearer {token}" or "token {token}"
+// useTempAcceptHeader some github api's require a custom "Accept" header, if this is true will set that custom header
+// body when wanting to send request body (mainly used when verb is "POST")
+func (g *github) callGithub(url, verb, authHeaderValue string, useTempAcceptHeader bool, body interface{}) ([]byte, error) {
+	klog.Debugf("Full Github URL: %v, Auth Token: %v", url, authHeaderValue)
 	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseRequest(req)
 	defer fasthttp.ReleaseResponse(resp)
 
-	req.SetRequestURI(fullUrl)
+	req.SetRequestURI(url)
 	req.Header.SetMethod(verb)
-	req.Header.Set("Accept", g.acceptHeader)
-	req.Header.Set("Authorization", authToken)
+
+	if useTempAcceptHeader {
+		req.Header.Set("Accept", TEMP_ACCEPT_HEADER_VALUE)
+	}
+
+	if authHeaderValue != "" {
+		req.Header.Set("Authorization", authHeaderValue)
+	}
+	req.Header.SetContentType("application/json")
+	if body != nil {
+		bodyBytes, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		req.SetBody(bodyBytes)
+	}
 
 	err := fasthttp.Do(req, resp)
 	if err != nil {
@@ -62,61 +191,10 @@ func (g *github) CallGithub(endpoint, verb, query, authToken string) ([]byte, er
 	return resp.Body(), nil
 }
 
-// A function that returns installation access token (different from JWT token)
-// That token is used in most api calls to identify the org the user belongs to
-// Calls generateJWTToken() and calls a github endpoint to generate the token
-// Note: whenever installation token is used in any endpoint, need to use "token" in
-// "Authorization" header instead of "Bearer"
-func (g *github) GenerateInstallationToken(installationId string) (string, error) {
-	endpoint := fmt.Sprintf("/app/installations/%v/access_tokens", installationId)
-
-	jwtToken, err := g.generateJWTToken()
-	if err != nil {
-		return "", err
-	}
-
-	body, err := g.CallGithub(endpoint, "POST", "", g.GenBearer(jwtToken))
-	if err != nil {
-		return "", err
-	}
-
-	type installationTokenModel struct {
-		Token string `json:"token"`
-	}
-	installationToken := installationTokenModel{}
-	err = json.Unmarshal(body, &installationToken)
-	if err != nil {
-		return "", err
-	}
-
-	return installationToken.Token, nil
+func genAuthToken(token string) string {
+	return fmt.Sprintf("token %v", token)
 }
 
-// A function that returns a JWT token that is valid for 10m
-// It uses the github app private key & app id (stored in config)
-func (g *github) generateJWTToken() (string, error) {
-	claims := jwt.StandardClaims{
-		IssuedAt:  time.Now().Unix(),
-		ExpiresAt: time.Now().Add(time.Minute * 10).Unix(),
-		Issuer:    g.appID,
-	}
-
-	signKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(g.appPrivateKey))
-	if err != nil {
-		klog.Fatalf("can't parse github app private key: %v", err)
-		return "", err
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	signedToken, err := token.SignedString(signKey)
-	if err != nil {
-		klog.Fatalf("can't sign the JWT token with the github app private key: %v", err)
-		return "", err
-	}
-
-	return signedToken, nil
-}
-
-func (g *github) GenBearer(jwtToken string) string {
+func genAuthBearer(jwtToken string) string {
 	return fmt.Sprintf("Bearer %v", jwtToken)
 }
